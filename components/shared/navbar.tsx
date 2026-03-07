e16b686f3045ac1a43234d38a6ddbb6948702514
brianstittsr <brianstittsr@gmail.com>
2026-03-07 10:18:59 -0500

    chore: Hide IAEOZ Summit Videos link from navbar resources menu

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -78,7 +78,7 @@ const resources = [
   { title: "News", href: "/news", icon: Newspaper },
   { title: "Press Releases", href: "/press-releases", icon: Radio },
   { title: "Video & Media", href: "/video-media", icon: Video },
-  { title: "IAEOZ Summit Videos", href: "/iaeoz-summit", icon: Play },
+  // { title: "IAEOZ Summit Videos", href: "/iaeoz-summit", icon: Play },
   { title: "Events", href: "/events", icon: Calendar },
   { title: "CMMC", href: "/cmmc", icon: Shield },
   { title: "Industries", href: "/industries", icon: Factory },
